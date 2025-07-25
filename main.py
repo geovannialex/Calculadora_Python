9318c1ef78d395196290ce02afe82006879ef2cb
GeovanniAlex <geovannialexandre2@gmail.com>
2025-07-24 21:26:36 -0300

    Separação de tela e definição das cores

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 
+# cores
+
+cor1 = "#3b3b3b" # black
+cor2 = "#feffff" # white
+cor3 = "#38576b" # blue
+cor4 = "#ECEFF1" # gray
+cor5 = "#FFAB40" # orange
+
 janela = Tk()
 janela.title("Calculadora")
 janela.geometry("300x400")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=300, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=300, height=350)
+frame_corpo.grid(row=1, column=0)
 
 janela.mainloop()
